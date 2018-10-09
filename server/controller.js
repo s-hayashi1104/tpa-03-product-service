a6dd84532fa805a34416ee79f9d23b568b01a513
s-hayashi1104 <shibahas.has@gmail.com>
2018-10-09 19:43:32 +0900

    json get して画面描画は完了 ただ本来であればproduct.jsを使うべきところだと思う その後の合計金額を算出するところもproduct.js使いそうなので早めのプルリク
    product.jsをrequireで呼び出して使おうとするとis not function と怒られる

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,19 @@
-//
-// TODO
-//
-// APIルーターのハンドラーをここに定義して実装する
-//
+const getTotalPrice = require('./product');
+const getProducts = require('./product');
+const productsList = require('./data/products.json');
+
+const getProductsHandler = function(req, res) {
+  const obj = { 'products': getProducts };
+  res.json(obj);
+};
+
+const calcTotalPriceHandler = function(req, res) {
+  const selectIds = req.query.ids;
+  const price = getTotalPrice(selectIds);
+  res.json(price);
+};
+
+module.exports = {
+  getProductsHandler,
+  calcTotalPriceHandler,
+};
--- server/setup.js
+++ server/setup.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const {
+  getProductsHandler,
+  calcTotalPriceHandler,
+} = require('./controller');
 
 const setup = (app) => {
   const apiRouter = express.Router();
-
-  //
-  // TODO
-  //
-  // apiRouter にAPIルートとハンドラーを設定する
-  //
+  apiRouter.get('/products', getProductsHandler);
+  apiRouter.get('/price', calcTotalPriceHandler);
 
   app.use('/api', apiRouter);
 };
