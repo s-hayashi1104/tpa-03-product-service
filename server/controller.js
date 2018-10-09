f165df0a55b87109c44bcc8b217273f6269c0a72
s-hayashi1104 <shibahas.has@gmail.com>
2018-10-09 20:39:28 +0900

    選択した商品の合計金額を返すように改修 クエリストリングがids以外の場合400を返すように改修

--- server/controller.js
+++ server/controller.js
@@ -1,16 +1,36 @@
-const getTotalPrice = require('./product');
-// const getProducts = require('./product');
-const productsList = require('./data/products.json');
+const {
+  getTotalPrice,
+  getProducts,
+} = require('./product');
 
 const getProductsHandler = function(req, res) {
-  const obj = { 'products': productsList };
+  const obj = { 'products': getProducts() };
   res.json(obj);
 };
 
 const calcTotalPriceHandler = function(req, res) {
-  const selectIds = req.query.ids;
+  let flag = false;
+  const selectIds = req.query.ids.split(',');
+  const products = getProducts();
+  selectIds.forEach((element) => {
+    const num = parseInt(element, 10);
+    if (Number.isNaN(num)) {
+      flag = true;
+    }
+    if (products.length < num) {
+      flag = true;
+    }
+  });
+
+  if (flag) return res.status(400).json({});
+
   const price = getTotalPrice(selectIds);
-  res.json(price);
+
+  const obj = {
+    'productIds': selectIds,
+    'total': price,
+  };
+  res.json(obj);
 };
 
 module.exports = {
